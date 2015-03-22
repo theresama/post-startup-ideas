a59c10637cb5a5370b9f077ae938f9430d6fe32f
Theresa Ma <theresa.ma@mail.utoronto.ca>
2015-03-21 23:37:32 -0400

    user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get 'pages/home'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   
   resources :users
 
