7b3a594df0dce296878cc56e01d715192a7d880b
Theresa Ma <theresa.ma@mail.utoronto.ca>
2015-03-21 23:00:08 -0400

    user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
+
+  root 'pages#home'
+
+  get 'pages/home'
+  get 'signup'  => 'users#new'
+  
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
