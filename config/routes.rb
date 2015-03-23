6348dfa3641a8ccdee21fd5856793f44246132df
Theresa Ma <theresa.ma@mail.utoronto.ca>
2015-03-23 17:25:31 -0400

    search tags

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     member do
       put "like", to: "ideas#like"
       put "dislike", to: "ideas#dislike"
+      #get "search", to: "ideas#index", as: :tag
     end
     
   end
